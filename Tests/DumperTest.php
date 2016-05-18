432d5bdcaaae73fa771af8d265cf4a6e37431b50
chrisyue <blizzchris@gmail.com>
2016-05-18 14:32:06 +0800

    remove uri processor

--- Tests/DumperTest.php
+++ Tests/DumperTest.php
@@ -12,16 +12,14 @@
 namespace Tests;
 
 use Chrisyue\PhpM3u8\Dumper;
-use Prophecy\Argument;
 
 class DumperTest extends \PHPUnit_Framework_TestCase
 {
     public function testDump()
     {
         $m3u8 = DummyM3u8Factory::createM3u8();
-        $uriProcessor = $this->prophesizeUriProcessor();
 
-        $dumper = new Dumper($uriProcessor->reveal());
+        $dumper = new Dumper();
         $m3u8Content = $dumper->dump($m3u8);
 
         $this->assertEquals($m3u8Content, DummyM3u8Factory::createM3u8Content());
@@ -30,23 +28,10 @@ class DumperTest extends \PHPUnit_Framework_TestCase
     public function testDumpVersionLessThan3()
     {
         $m3u8 = DummyM3u8Factory::createM3u8(2);
-        $uriProcessor = $this->prophesizeUriProcessor();
 
-        $dumper = new Dumper($uriProcessor->reveal());
+        $dumper = new Dumper();
         $m3u8Content = $dumper->dump($m3u8);
 
         $this->assertEquals($m3u8Content, DummyM3u8Factory::createM3u8Content(2));
-    }
-
-    private function prophesizeUriProcessor()
-    {
-        $uriProcessor = $this->prophesize('Chrisyue\PhpM3u8\M3u8\MediaSegment\UriProcessor\UriProcessorInterface');
-
-        $uriProcessor->process(Argument::type('Chrisyue\PhpM3u8\M3u8\MediaSegment\MediaSegment'))
-            ->shouldBeCalled()->will(function ($args) {
-                return $args[0]->getUri();
-            });
-
-        return $uriProcessor;
     }
 }
