ad40811cd5ebc4888699b84407b89653b3fe337f
medved <medved@DESKTOP-3U6OTST>
2017-02-07 01:49:01 +0300

    fix: parse name and group from m3u8 file

--- M3u8/MediaSegment/MediaSegment.php
+++ M3u8/MediaSegment/MediaSegment.php
@@ -13,36 +13,51 @@ namespace Chrisyue\PhpM3u8\M3u8\MediaSegment;
 
 class MediaSegment implements MediaSegmentInterface
 {
-    protected $uri;
-    protected $duration;
-    protected $sequence;
-    protected $isDiscontinuity;
+	protected $uri;
+	protected $duration;
+	protected $sequence;
+	protected $name;
+	protected $group;
+	protected $isDiscontinuity;
 
-    public function __construct($uri, $duration, $sequence, $isDiscontinuity = false)
-    {
-        $this->uri = $uri;
-        $this->duration = $duration;
-        $this->sequence = $sequence;
-        $this->isDiscontinuity = $isDiscontinuity;
-    }
+	public function __construct($uri, $duration, $sequence, $name, $group, $isDiscontinuity = false)
+	{
+		$this->uri = $uri;
+		$this->duration = $duration;
+		$this->sequence = $sequence;
+		$this->name = $name;
+		$this->group = $group;
+		$this->isDiscontinuity = $isDiscontinuity;
+	}
 
-    public function getUri()
-    {
-        return $this->uri;
-    }
+	public function getUri()
+	{
+		return $this->uri;
+	}
 
-    public function getDuration()
-    {
-        return $this->duration;
-    }
+	public function getDuration()
+	{
+		return $this->duration;
+	}
 
-    public function getSequence()
-    {
-        return $this->sequence;
-    }
+	public function getName()
+	{
+		return $this->name;
+	}
 
-    public function isDiscontinuity()
-    {
-        return $this->isDiscontinuity;
-    }
+	public function getGroup()
+	{
+		return $this->group;
+	}
+
+	public function getSequence()
+	{
+		return $this->sequence;
+	}
+
+	public function isDiscontinuity()
+	{
+		return $this->isDiscontinuity;
+	}
+
 }
